9d46631b4acc55ed38feaf45cade0b9e477610d5
murilodospacos <murilopacos@gmail.com>
2021-10-18 22:08:48 -0300

    requisito 1 tentativa 1

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -3,10 +3,10 @@ const Products = require('../services/productsServices');
 const getAllProducts = async (_req, res) => {
   try {
     const products = await Products.getAllProducts();
-    res.status(200).json(products);
+    return res.status(200).json(products);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Aconteceu erro ao buscar os dados' });
+    return res.status(500).json({ message: 'Aconteceu erro ao buscar os dados' });
   }
 };
 
@@ -17,9 +17,9 @@ const createProducts = async (req, res) => {
     // if (data.error_code && data.error_code === 'PRODUCTS_EXISTS') {
     //   return res.status(400).json({ message: 'Esse produto já está cadastrado' });
     // }
-    res.status(201).json(data);
+    return res.status(201).json(data);
   } catch (error) {
-    res.status(500).json({ message: 'Aconteceu erro ao cadastrar o som' });
+    return res.status(500).json({ message: 'Aconteceu erro ao cadastrar o som' });
   }
 };
 
--- models/connections.js
+++ models/connections.js
@@ -1,23 +1,42 @@
+// const { MongoClient } = require('mongodb');
+
+// const OPTIONS = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// };
+
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+// let db = null;
+// // mongodb://mongodb:27017/StoreManager
+
+// const connection = () => (
+//    db
+//     ? Promise.resolve(db)
+//     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+//     .then((conn) => {
+//     db = conn.db('StoreManager');
+//     return db;
+//     })
+// );
+
+// module.exports = connection;
+
 const { MongoClient } = require('mongodb');
 
-const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-
-let db = null;
-// mongodb://mongodb:27017/StoreManager
-
-const connection = () => (
-   db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-    db = conn.db('StoreManager');
-    return db;
-    })
-);
+const connection = () =>
+MongoClient
+.connect(MONGO_DB_URL, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+.then((conn) => conn.db('StoreManager'))
+.catch((err) => {
+console.error(err);
+process.exit(1);
+});
 
 module.exports = connection;
