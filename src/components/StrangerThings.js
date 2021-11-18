d61a8213257cc680f0f2eac8826684a2e4c15e28
Murilo Paços <murilopacos@gmail.com>
2021-11-18 15:29:14 -0300

    corrigindo erro de variavel de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -13,7 +13,7 @@ const strangerThingsConfig = {
 
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
-  timeout: JSON.parse(process.env.REACT_APP_UPSIDEDOWN_URL),
+  timeout: JSON.parse(process.env.REACT_APP_UPSIDEDOWN_TIMEOUT),
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
