c611903cf15fcc393b1b00fd3602e5d72145b664
Rodolfo <rodolforrcont@gmail.com>
2022-01-04 20:49:46 -0300

    Requisito 1 - Rota de login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
+import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -9,6 +11,13 @@ function App() {
       <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
         Glass
       </object>
+      <BrowserRouter>
+        <Switch>
+          <Route>
+            <Redirect exact from="/" to="/login" component={Login} />
+          </Route>
+        </Switch>
+    </BrowserRouter>
     </div>
   );
 }
