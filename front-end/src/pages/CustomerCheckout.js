29b04184e9096af4b986016a33ea067691139750
Murilo Paços <murilopacos@gmail.com>
2022-01-09 14:48:57 -0300

    adicionando component navbar na pagina

--- front-end/src/pages/CustomerCheckout.js
+++ front-end/src/pages/CustomerCheckout.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import DeliveryDetailsAddress from '../components/customer-check/DeliveryDetailsAddress';
 import FinalizeOrder from '../components/customer-check/FinalizeOrder';
-// import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar';
 
 function CustomerCheckout() {
   const [products, setProducts] = useState([]);
@@ -15,12 +15,12 @@ function CustomerCheckout() {
   }
 
   return (
-    <>
-      {/* <Navbar /> */}
+    <div>
+      <Navbar />
       <FinalizeOrder products={ products } />
       <DeliveryDetailsAddress />
       <button type="button" onClick={ getProducts }>GET PRODUCTS</button>
-    </>
+    </div>
   );
 }
 
