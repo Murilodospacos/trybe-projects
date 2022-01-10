4b597ee2283ebd1d89ddfb434e38853d735faf22
Murilo Paços <murilopacos@gmail.com>
2022-01-10 18:31:44 -0300

    adiciona lógica remove item, soma produtos

--- front-end/src/components/customer-check/FinalizeOrder.js
+++ front-end/src/components/customer-check/FinalizeOrder.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext, useEffect } from 'react';
+import { ProductContext } from '../../provider/ProductContext';
 
-export default function FinalizeOrder({ products }) {
-  // const { products } = this.props;
+export default function FinalizeOrder() {
+  const { cart, setCart } = useContext(ProductContext);
+
+  function deleteProducts(id) {
+    const newArray = cart.filter((item) => item.id !== id);
+    setCart(newArray);
+    return newArray;
+  }
+
+  function totalPrice() {
+    const result = cart
+      .reduce((total, curr) => total + parseFloat(curr.price * curr.quantity), 0)
+      .toFixed(2);
+    return result;
+  }
+
+  useEffect(() => {
+    setCart(cart);
+    console.log(cart, 'carttt');
+  }, [cart]);
+
   return (
     <section>
       <h2>Finalizar Pedido</h2>
@@ -17,38 +36,57 @@ export default function FinalizeOrder({ products }) {
             <th>Remover Item</th>
           </tr>
         </thead>
-        { products.map((item, index) => (
+        { cart.map((item, index) => (
           <tbody key={ index }>
             <tr>
-              <td data-testid={ `customer_checkout__element-order-table-${index}-` }>
+              <td
+                data-testid={ `customer_checkout__element-order-table-${index}-` }
+              >
                 {index + 1}
               </td>
-              <td data-testid="customer_checkout__element-order-table-name-">
+              <td
+                data-testid={ `customer_checkout__element-order-table-name-${index}` }
+              >
                 { item.name }
               </td>
-              <td data-testid="customer_checkout__element-order-table-quantity-">
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-quantity-${index}`
+                }
+              >
                 { item.quantity }
               </td>
-              <td data-testid="customer_checkout__element-order-table-unit-price-">
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-unit-price-${index}`
+                }
+              >
                 { item.price }
               </td>
-              <td data-testid="customer_checkout__element-order-table-sub-total-">
-                R$10,50
+              <td
+                data-testid={
+                  `customer_checkout__element-order-table-sub-total-${index}`
+                }
+              >
+                { (item.price * item.quantity).toFixed(2) }
               </td>
-              <td data-testid="customer_checkout__element-order-table-remove-">
-                Remover
+              <td
+                data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+              >
+                <button
+                  type="button"
+                  onClick={ () => deleteProducts(item.id) }
+                >
+                  Remover
+                </button>
               </td>
             </tr>
           </tbody>
         )) }
       </table>
       <h1 data-testid="customer_checkout__element-order-total-price">
-        Total: R$  28,46
+        { `Total: R$ ${totalPrice()}` }
       </h1>
     </section>
   );
 }
-
-FinalizeOrder.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.name).isRequired,
-};
--- front-end/src/pages/CustomerCheckout.js
+++ front-end/src/pages/CustomerCheckout.js
@@ -1,25 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import DeliveryDetailsAddress from '../components/customer-check/DeliveryDetailsAddress';
 import FinalizeOrder from '../components/customer-check/FinalizeOrder';
 import Navbar from '../components/Navbar';
 
 function CustomerCheckout() {
-  const [products, setProducts] = useState([]);
-
-  function getProducts() {
-    axios.get('http://localhost:3001/products')
-      .then((response) => {
-        setProducts(response.data);
-      });
-  }
-
   return (
     <div>
       <Navbar />
-      <FinalizeOrder products={ products } />
+      <FinalizeOrder />
       <DeliveryDetailsAddress />
-      <button type="button" onClick={ getProducts }>GET PRODUCTS</button>
     </div>
   );
 }
