a8cbf1dd5fc9a5ce9dec477ca19f674957bf8d03
Rodolfo <rodolforrcont@gmail.com>
2022-01-10 14:56:04 -0300

    Requisito 15 - Primeiro teste

--- front-end/src/components/CardProduct.js
+++ front-end/src/components/CardProduct.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ProductContext } from '../provider/ProductContext';
+
+const magicNumber = -1;
 
 function CardProduct({ id, name, price, urlImage, key }) {
-  console.log(urlImage);
+  const { cart, setCart } = React.useContext(ProductContext);
+  const [count, setCount] = React.useState(magicNumber);
+
+  React.useEffect(() => {
+    const productExist = cart.findIndex((value) => value.id === id);
+    if (count <= 0) {
+      setCart([...cart.filter((_, index) => productExist !== index)]);
+      return;
+    }
+    if (productExist >= 0) {
+      setCart([...cart.map((element, index) => {
+        if (productExist === index) {
+          element.quantity = count;
+        }
+        return element;
+      })]);
+      return;
+    }
+    setCart([...cart, { id, name, price, quantity: count }]);
+  }, [count]);
+
+  console.log(cart);
+  const handleIncrease = () => {
+    if (count === magicNumber) {
+      setCount(1);
+    } else {
+      setCount(count + 1);
+    }
+  };
+
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div key={ key }>
       <p data-testid={ `customer_products__element-card-price-${id}` }>
@@ -19,16 +57,18 @@ function CardProduct({ id, name, price, urlImage, key }) {
         <button
           type="button"
           data-testid={ `customer_products__button-card-add-item-${id}` }
+          onClick={ handleDecrease }
         >
           -
         </button>
         <input
           data-testid={ `customer_products__input-card-quantity-${id}` }
-          defaultValue={ 0 }
+          value={ count === magicNumber ? 0 : count }
         />
         <button
           type="button"
           data-testid={ `customer_products__button-card-rm-item-${id}` }
+          onClick={ handleIncrease }
         >
           +
         </button>
