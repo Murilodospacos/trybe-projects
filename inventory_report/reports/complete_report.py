6da254e3f7b3482d8ad9c412119f470c8fd3cd9e
Murilo Paços <murilopacos@gmail.com>
2022-02-02 09:29:13 -0300

    requisito 2 - 1ºtentativa

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,35 +1,18 @@
-import json
-# from .simple_report import SimpleReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
-class CompleteReport:
+class CompleteReport():
     @classmethod
     def generate(cls, inventory):
-        data = json_reader(inventory)
-        # simple_repo = SimpleReport.generate(data)
+        data = inventory
+        simple_repo = SimpleReport.generate(data)
         factory = {}
         for item in data:
             if item["nome_da_empresa"] not in factory:
                 factory[item["nome_da_empresa"]] = 1
             else:
                 factory[item["nome_da_empresa"]] += 1
-
-        # print(dir(factory))
+        response = ''
         for chave, fac in factory.items():
-            print(f"-{chave}: {fac}")
-
-        # return (
-        #         f"Produtos estocados por empresa: \n"
-        #         f"- {factory[0]}\n"
-        #         f"- {factory[1]}\n"
-        #         f"- {factory}\n"
-        # )
-
-
-def json_reader(file):
-    with open(file) as f:
-        itens = json.load(f)
-    return itens
-
-
-print(CompleteReport.generate("inventory.json"))
+            response += f"- {chave}: {fac}\n"
+        return simple_repo + "\nProdutos estocados por empresa: \n" + response
