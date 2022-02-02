7d085e1b8ee483f72a3289a3f01cee06b69c5f85
marcoswarmling <marcoswarmling15@gmail.com>
2022-02-02 15:31:27 -0300

    requisitos 3,4 e 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,34 @@
-# import json
+from csv import DictReader
+from json import load
+import xml.etree.ElementTree as ET
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
-# def json_reader(file):
-#     with open(file) as f:
-#         itens = json.load(f)
-#     return itens
 
+class Inventory:
+    @classmethod
+    def read_files(cls, path, file):
+        if path.endswith(".csv"):
+            return DictReader(file, delimiter=",", quotechar='"')
+        elif path.endswith(".json"):
+            return load(file)
+        else:
+            tree = ET.parse(path)
+            root = tree.getroot()
+            xml_list = []
+            for child in root:
+                xml_dict = {}
+                for i in range(len(child)):
+                    xml_dict[child[i].tag] = child[i].text
+                xml_list.append(xml_dict)
+        return xml_list
 
-# print(CompleteReport.generate("inventory.json"))
+    @classmethod
+    def import_data(cls, path, type):
+        with open(path, "r") as f:
+            items = cls.read_files(path, f)
+            if type == "simples":
+                result = SimpleReport().generate(list(items))
+            if type == "completo":
+                result = CompleteReport().generate(list(items))
+        return result
