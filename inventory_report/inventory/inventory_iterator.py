ac7d41c591779454390bf6dcb67730ba733c20f1
marcoswarmling <marcoswarmling15@gmail.com>
2022-02-03 16:56:36 -0300

    requisito 8

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -5,7 +5,7 @@ class InventoryIterator(Iterator):
     def __init__(self, list):
         self.i = 0
         self.list = list
- 
+
     def __next__(self):
         if self.i == len(self.list):
             raise StopIteration
--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -3,14 +3,20 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 
+class InventoryRefactor(InventoryIterator):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
 
-class InventoryRefactor:
-    @classmethod
-    def import_data(cls, path, type):
-        with open(path, "r") as f:
-            items = cls.read_files(path, f)
-            if type == "simples":
-                result = SimpleReport().generate(list(items))
-            if type == "completo":
-                result = CompleteReport().generate(list(items))
-        return result
+    def import_data(self, path, type):
+        report_data = self.importer.import_data(path)
+
+        self.data.extend(report_data)
+
+        if type == "simples":
+            return SimpleReport().generate(list(report_data))
+        if type == "completo":
+            return CompleteReport().generate(list(report_data))
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
