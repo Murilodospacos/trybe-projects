77aa54abf490b750e1c44afb70d3ed6afbc5e787
Murilo Paços <murilopacos@gmail.com>
2022-02-21 19:03:49 -0300

    requisito 1 - 6° tentativa

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,5 +2,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == '':
         return False
     elif len(word) < high_index:
-        return True
+        return word
     return is_palindrome_recursive(word, low_index, len(word) - 1)
