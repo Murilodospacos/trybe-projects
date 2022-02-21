e7ee05653ddf2cd05a9e4b297fc0bbfd9f58babe
Murilo Paços <murilopacos@gmail.com>
2022-02-21 18:54:12 -0300

    requisito 1 - 4°tentativa

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    print("t11")
+    if word == '':
+        return False
+    elif len(word) < high_index:
+        return True
+    return is_palindrome_recursive(word, low_index, len(word) - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,12 @@
+# https://stackoverflow.com/questions/13628791/determine-whether-integer-is-between-two-other-integers
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
-    else:
-        count = 0
-        for item in permanence_period:
-            if type(item[0]) != int or type(item[1]) != int:
-                return None
-            elif item[0] <= target_time <= item[1]:
-                count += 1
+    count = 0
+    for item in permanence_period:
+        if not isinstance(item[0], int) or not isinstance(item[1], int):
+            return None
+        elif item[0] <= target_time <= item[1]:
+            count += 1
 
     return count
