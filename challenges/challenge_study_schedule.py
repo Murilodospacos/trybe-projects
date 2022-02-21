b78c912974b61a29c656e542f144cfba833b5c50
Murilo Paços <murilopacos@gmail.com>
2022-02-21 18:57:47 -0300

    requisito 1 - 5° tentativa

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,6 +4,8 @@ def study_schedule(permanence_period, target_time):
         return None
     count = 0
     for item in permanence_period:
+        # https://www.w3schools.com/python/ref_func_isinstance.asp
+        # Tive ajuda da Lena e do Mauricio, pois com type/int não passou
         if not isinstance(item[0], int) or not isinstance(item[1], int):
             return None
         elif item[0] <= target_time <= item[1]:
