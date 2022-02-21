e25f0bd667cd91b94778fe1ed29b20b2f5c66fe4
Murilo Paços <murilopacos@gmail.com>
2022-02-21 17:34:35 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    print("T11")
+    if not target_time:
+        return None
+    else:
+        count = 0
+        for item in permanence_period:
+            if type(item[0]) != int or type(item[1]) != int:
+                return None
+            elif item[0] <= target_time <= item[1]:
+                count += 1
+
+    return count
