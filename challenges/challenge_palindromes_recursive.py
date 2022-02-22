704bee39ae7cb6c0062fc43a341adbc2b01cc450
Murilo Paços <murilopacos@gmail.com>
2022-02-22 17:42:32 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if word == '':
         return False
-    elif len(word) < high_index:
-        return word
-    return is_palindrome_recursive(word, low_index, len(word) - 1)
+    # https://www.w3schools.com/python/python_howto_reverse_string.asp
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
