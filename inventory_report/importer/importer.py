ad07de1dd684d03bca3ae530f0df0966b0b2f9f8
Murilo Paços <murilopacos@gmail.com>
2022-02-28 13:44:44 -0300

    projeto

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -4,4 +4,6 @@ from abc import ABC, abstractmethod
 class Importer(ABC):
     @abstractmethod
     def import_data(self, fileName):
+        # https://airbrake.io/blog/python/python-exception-handling-notimplementederror
+        # Quando a implementação ainda está faltando
         raise NotImplementedError
