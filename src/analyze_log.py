00f52cfe5c2b871e691a19c59fb510baed397ca1
Murilo Paços <murilopacos@gmail.com>
2022-03-07 14:17:56 -0300

    Iniciando projeto

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,27 @@
+import csv
+
+# https://stackoverflow.com/questions/24662571/python-import-csv-to-list
+
+
+fileTXT = "data/mkt_campaign.txt"
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(fileTXT, "w") as txtFile:
+        with open(path_to_file, "r") as csvFile:
+            if path_to_file.endswith(".csv"):
+                [
+                    txtFile.write(" ".join(row) + "\n")
+                    for row in csv.reader(csvFile)
+                ]
+                txtFile.close()
+            else:
+                raise FileNotFoundError(f"Arquivo: {path_to_file} inválido")
+
+
+# with open(path_to_file) as csvFile:
+#             if path_to_file.endswith(".csv"):
+#                 csvReader = csv.DictReader(csvFile)
+#                 data = list(csvReader)
+#             else:
+#                 raise FileNotFoundError(f"Arquivo: {path_to_file} inválido")
