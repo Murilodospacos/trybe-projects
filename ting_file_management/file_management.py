6d8d9f5623e93e810b67d806a8db282f47ecd623
Murilo Paços <murilopacos@gmail.com>
2022-03-10 17:23:57 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as txtFile:
+            # https://stackoverflow.com/questions/12330522/how-to-read-a-file-without-newlines
+            data = txtFile.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        return data
+
+
+# txtFile.write("Formato inválido")
+
+
+print(txt_importer("statics/arquivo_teste.txt"))
