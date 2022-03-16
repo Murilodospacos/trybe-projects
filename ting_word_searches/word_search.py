c8426dbcf829743e9db13bf59d8a42b8a847f68e
Murilo Paços <murilopacos@gmail.com>
2022-03-15 21:00:22 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,20 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lista = list()
+    for index in range(len(instance)):
+        path_file = instance.search(index)
+        content_file = txt_importer(path_file)
+        ocurrencies_list = list()
+        for index, line in enumerate(content_file):
+            if word.lower() in line.lower():
+                ocurrencies_list.append({"linha": index + 1, "conteudo": line})
+        if ocurrencies_list:
+            lista.append(
+                {
+                    "palavra": word,
+                    "arquivo": path_file,
+                    "ocorrencias": ocurrencies_list,
+                }
+            )
+    return lista
